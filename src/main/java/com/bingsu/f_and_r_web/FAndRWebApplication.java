743c0efec422a36fbc6038b3b8831a3cbe8c0d5b
Bing-su <57391343+Bingsu-kun@users.noreply.github.com>
2021-01-22 22:51:52 +0900

    Initial commit

---
+++ src/main/java/com/bingsu/f_and_r_web/FAndRWebApplication.java
@@ -0,0 +1,13 @@
+package com.bingsu.f_and_r_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FAndRWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FAndRWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bingsu/f_and_r_web/FAndRWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bingsu.f_and_r_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FAndRWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
