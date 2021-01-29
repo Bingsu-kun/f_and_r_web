de21ed0aae153a18773a0ecb87a2bf027dcd1373
bingsu <icetime963@gmail.com>
2021-01-29 23:17:34 +0900

    Add OAuth2 Back

---
+++ src/main/java/com/bingsu/f_and_r_web/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package com.bingsu.f_and_r_web.config.auth.dto;
+
+import com.bingsu.f_and_r_web.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
